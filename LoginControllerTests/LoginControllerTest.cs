f9596008c0d535cc0a423499276af4ee6130c2aa
Andriy Korchynskyy <andriy.korchynskyy@gmail.com>
2018-12-11 15:27:32 +0200

    Closes #22 - Add folders: Constants, Models, Tests

---
+++ LoginControllerTests/LoginControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Net;
+using System.Net.Http;
+using Xunit;
+using gtd_timer;
+using NUnit.Framework;
+
+namespace LoginControllerTests
+{
+    public class LoginControllerTest
+    {
+        private readonly HttpClient httpClient;
+        private readonly TokenClient tokenClient;
+
+        public LoginControllerTest()
+        {
+            var webhost = new WebHostBuilder()
+            .UseUrls("http://*:8000")
+            .UseStartup<Startup>();
+
+            var server = new TestServer(webhost);
+            this.httpClient = server.CreateClient();
+
+            var disco = DiscoveryClient.GetAsync("http://localhost:5000").Result;
+            this.tokenClient = new TokenClient(disco.TokenEndpoint, "name", "password");
+        }
+
+        [Fact]
+        public async void ShouldNotAllowAnonymousUser()
+        {
+            var result = await httpClient.GetAsync("http://localhost:44398/api/login");
+            Xunit.Assert.Equal(HttpStatusCode.Unauthorized, result.StatusCode);
+        }
+
+        [Fact]
+        public async void ShouldReturnValuesForAuthenticatedUser()
+        {
+            var tokenResponse = tokenClient.RequestResourceOwnerPasswordAsync("Bob", "password").Result;
+            httpClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var result = await httpClient.GetStringAsync("http://localhost:44398/api/login");
+            Xunit.Assert.Equal("[\"value1\",\"value2\"]", result);
+        }
+    }
+}
---
+++ gtd-timer/Constants/Constans.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gtdtimer.Constant
+{
+    public class Constans
+    {
+        public const string SecretKey = "MyKey";
+    }
+}
