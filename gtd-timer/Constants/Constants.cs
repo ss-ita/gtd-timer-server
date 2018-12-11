10df1b548fe454e93756cbccb0c6e0cbd7547779
Andriy Korchynskyy <andriy.korchynskyy@gmail.com>
2018-12-11 23:04:49 +0200

    Closes #22 - Add folders: Constants, Models, Tests

--- gtd-timer/Constants/Constants.cs
+++ gtd-timer/Constants/Constants.cs
@@ -4,5 +4,9 @@
     {
         public const string SecretKey = "MyKey";
         public const string RequiredUser = "Username is required";
+        public const int Status401Unauthorized = 401;
+        public const string CorectEmail = "example33@gmail.com";
+        public const string CorectPassword = "1234567";
+        public const int Status200OK = 401;
     }
 }
