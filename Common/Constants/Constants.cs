006a510fa023d78888f1e1c29e368a9c3e8ae24b
Oleksandr Tymoshchuk <37620894+AlexLviv07@users.noreply.github.com>
2018-12-28 11:12:06 +0200

    Closes #45 - Create unit tests for Login Service (#81)

--- Common/Constants/Constants.cs
+++ Common/Constants/Constants.cs
@@ -9,5 +9,7 @@
         public const int Validity = 1;
         public const string PasswordInvalidMessage = "The Password field is not a valid password";
         public const string PasswordRegularExpression = @"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=.,\-_!])([a-zA-Z0-9 @#$%^&+=*.,\-_!]){8,}$";
+        public const string ErrorMessageUserNotFound = "User does not Exist!";
+        public const string ErrorMessageIncorrectPassword = "Incorrect password entered";
     }
 }
---
+++ Tests/ServiceTierTests/LoginServiceTests.cs
@@ -0,0 +1,70 @@
+﻿using Common.Exceptions;
+using Common.Model;
+using gtdtimer.Services;
+using Microsoft.AspNet.Identity;
+using Moq;
+using NUnit.Framework;
+using Timer.DAL.Timer.DAL.Entities;
+using Timer.DAL.Timer.DAL.Repositories;
+using Timer.DAL.Timer.DAL.UnitOfWork;
+
+namespace ServiceTierTests
+{
+    class LoginServiceTests
+    {
+        private Mock<IUnitOfWork> unitOfWork;
+        private Mock<IApplicationUserManager<User, int>> userManager;
+        private LogInService subject;
+        private const string TokenPartTest = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+
+        [SetUp]
+        public void Setup()
+        {
+            unitOfWork = new Mock<IUnitOfWork>();
+            userManager = new Mock<IApplicationUserManager<User, int>>();
+            subject = new LogInService(unitOfWork.Object);
+        }
+
+        [Test]
+        public void CreateToken()
+        {
+            LoginModel model = new LoginModel { Email = "test@gmail.com", Password = "12345" };
+            User user = new User() { PasswordHash = "12345", Email = "test@gmail.com" };
+
+            userManager.Setup(_ => _.FindByEmailAsync(model.Email)).ReturnsAsync(user);
+            unitOfWork.Setup(_ => _.UserManager).Returns(userManager.Object);
+
+            var actual = subject.CreateToken(model);
+
+            Assert.AreEqual(actual.Contains(TokenPartTest), true);
+        }
+
+        [Test]
+        public void LoginTest_Throws_UserNotFoundException()
+        {
+            LoginModel model = new LoginModel { Email = "", Password = "12345" };
+            User user = null;
+
+            userManager.Setup(_ => _.FindByEmailAsync(model.Email)).ReturnsAsync(user);
+            unitOfWork.Setup(_ => _.UserManager).Returns(userManager.Object);
+
+            var ex = Assert.Throws<UserNotFoundException>(() => subject.CreateToken(model));
+
+            Assert.That(ex.Message, Is.EqualTo(Common.Constant.Constants.ErrorMessageUserNotFound));
+        }
+
+        [Test]
+        public void LoginTest_Throws_IncorrectPasswordException()
+        {
+            LoginModel model = new LoginModel { Email = "", Password = "12345" };
+            User user = new User() { PasswordHash = "1234" };
+
+            userManager.Setup(_ => _.FindByEmailAsync(model.Email)).ReturnsAsync(user);
+            unitOfWork.Setup(_ => _.UserManager).Returns(userManager.Object);
+
+            var ex = Assert.Throws<IncorrectPasswordException>(() => subject.CreateToken(model));
+
+            Assert.That(ex.Message, Is.EqualTo(Common.Constant.Constants.ErrorMessageIncorrectPassword));
+        }
+    }
+}
