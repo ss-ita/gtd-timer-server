76dc81494008bbf5d58b2a281fc43d577dc5c5ff
Oleksandr-Lytvyn <oleksandrlytvyn1223@gmail.com>
2019-01-31 17:19:50 +0200

    zip delete

--- Timer.DAL/Entities/TimerContext.cs
+++ Timer.DAL/Entities/TimerContext.cs
@@ -90,20 +90,6 @@ namespace GtdTimerDAL.Entities
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            //builder.Entity<UserRole>(userRole =>
-            //{
-            //    userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
-
-            //    userRole.HasOne(ur => ur.Role)
-            //        .WithMany(r => r.UserRoles)
-            //        .HasForeignKey(ur => ur.RoleId)
-            //        .IsRequired();
-
-            //    userRole.HasOne(ur => ur.User)
-            //        .WithMany(r => r.UserRoles)
-            //        .HasForeignKey(ur => ur.UserId)
-            //        .IsRequired();
-            //});
         }
     }
 }
