2675b82599aa2bdaee9bae5030e380f5aba10c79
Oleksandr-Lytvyn <oleksandrlytvyn1223@gmail.com>
2019-02-01 12:16:16 +0200

    clock delete

--- Common/Email/SendGrid/SendGridEmailSender.cs
+++ Common/Email/SendGrid/SendGridEmailSender.cs
@@ -10,11 +10,11 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-using SendGrid;
+
 using GtdCommon.IoC;
-using SendGrid.Helpers.Mail;
 using Newtonsoft.Json;
-using GtdCommon.IoC;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace GtdCommon.Email.SendGrid
 {
