346dc0afb76a24a0ddd1b66ab3219489a7fe7b3c
nataliapachva <natalia.pachva@gmail.com>
2019-02-11 01:12:20 +0200

    Closes #231 -  Add SignalR

---
+++ gtd-timer/Hubs/ITaskClient.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+
+using GtdCommon.ModelsDto;
+
+namespace GtdTimer.Hubs
+{
+    /// <summary>
+    /// interface for task hub
+    /// </summary>
+    public interface ITaskClient
+    {
+        /// <summary>
+        /// This event occurs when create a task.
+        /// </summary>
+        /// <param name="model">task model</param>
+        /// <returns></returns>
+        Task CreateTask(TaskDto model);
+
+        /// <summary>
+        /// This event occurs when start a task.
+        /// </summary>
+        /// <param name="model">task model</param>
+        /// <returns></returns>
+        Task StartTask(TaskDto model);
+
+        /// <summary>
+        /// This event occurs when pause a task.
+        /// </summary>
+        /// <param name="model">task model</param>
+        /// <returns></returns>
+        Task PauseTask(TaskDto model);
+
+        /// <summary>
+        /// This event occurs when reset a task.
+        /// </summary>
+        /// <param name="model">task model</param>
+        /// <returns></returns>
+        Task ResetTask(TaskDto model);
+
+        /// <summary>
+        /// This event occurs when delete a task.
+        /// </summary>
+        /// <param name="taskId">task id</param>
+        /// <returns></returns>
+        Task DeleteTask(int taskId);
+
+        /// <summary>
+        /// This event occurs when update a task.
+        /// </summary>
+        /// <param name="model">task model</param>
+        /// <returns></returns>
+        Task UpdateTask(TaskDto model);
+    }
+}
---
+++ gtd-timer/Hubs/TaskHub.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+using GtdCommon.ModelsDto;
+using GtdServiceTier.Services;
+
+namespace GtdTimer.Hubs
+{
+    /// <summary>
+    /// class for task hub
+    /// </summary>
+    [Authorize]
+    public class TaskHub: Hub<ITaskClient>
+    {
+        /// <summary>
+        /// Instance of task service
+        /// </summary>
+        private readonly ITaskService taskService;
+
+        /// <summary>
+        /// Instance of user identity service
+        /// </summary>
+        private readonly IUserIdentityService userIdentityService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TaskHub" /> class.
+        /// </summary>
+        /// <param name="taskService">instance of task service</param>
+        public TaskHub(ITaskService taskService, IUserIdentityService userIdentityService)
+        {
+            this.taskService = taskService;
+            this.userIdentityService = userIdentityService;
+        }
+
+        public async Task CreateTask(TaskDto model)
+        {
+            model.UserId = GetUserId();
+            taskService.CreateTask(model);
+            await Clients.Others.CreateTask(model);
+        }
+
+        public async Task StartTask(TaskDto model)
+        {
+            model.UserId = GetUserId();
+            taskService.StartTask(model);
+            await Clients.Others.StartTask(model);
+        }
+
+        public async Task PauseTask(TaskDto model)
+        {
+            model.UserId = GetUserId();
+            taskService.PauseTask(model);
+            await Clients.Others.PauseTask(model);
+        }
+
+        public async Task ResetTask(TaskDto model)
+        {
+            model.UserId = GetUserId();
+            taskService.ResetTask(model);
+            await Clients.Others.ResetTask(model);
+        }
+
+        public async Task DeleteTask(int taskId)
+        {
+            taskService.DeleteTaskById(taskId);
+            await Clients.Others.DeleteTask(taskId);
+        }
+
+        public async Task UpdateTask(TaskDto model)
+        {
+            model.UserId = GetUserId();
+            taskService.UpdateTask(model);
+            await Clients.Others.UpdateTask(model);
+        }
+
+        private int GetUserId()
+        {
+            var userId = userIdentityService.GetUserId(Context.User.Identity);
+
+            return userId;
+        }
+    }
+}
